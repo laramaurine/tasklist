37b4d182f5942de89ab748c9e43260c411c34006
Lara Lesar <lara.m.lesar@gmail.com>
2020-11-20 16:06:35 -0600

    click listener for addTask woking

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,9 +4,15 @@ $( document ).ready( handleReady );
 function handleReady(){
     console.log('jquery has joined the party');
     //click listeners will go here
+    setupClickListeners();
 }
-
-//function addTask -- add new task
+//function for click listeners
+function setupClickListeners(){
+    $('#addTask').on('click', function(){
+        console.log('in addTask on click');
+    })
+}
+//function addTask -- add new task POST
 
 //function completeTask -- mark as complete
 
@@ -14,4 +20,4 @@ function handleReady(){
 
 //function refreshTasks -- refresh on reload
 
-//function renderTasks -- render on DOM
+//function renderTasks -- render on DOM --GET
