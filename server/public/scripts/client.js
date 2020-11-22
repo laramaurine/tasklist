60db0679fbeb1dc5b9ecb325a0753014bfb655cc
Lara Lesar <lara.m.lesar@gmail.com>
2020-11-21 20:14:20 -0600

    successfully adding data to database

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,11 +5,54 @@ function handleReady(){
     console.log('jquery has joined the party');
     //click listeners will go here
     setupClickListeners();
+    
 }
 //function for click listeners
 function setupClickListeners(){
     $('#addTask').on('click', function(){
         console.log('in addTask on click');
+        let taskToSend = {
+            task: $('#taskIn').val()
+        }
+        saveTask( taskToSend);
+    })
+}
+function saveTask( newTask ){
+    console.log('in save task', newTask);
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: newTask
+    })
+    .then( function(response) {
+        getTask();
+    })
+    .catch(function(error){
+        console.log('ERROR TRY AGAIN', error);
+        alert('ERROR TRY AGAIN')
+    })
+
+}
+function getTask(){
+    console.log('in function get task');
+    $.ajax({
+        type: 'GET',
+        url: '/tasks',
+    }).then( function (result) {
+        $('#viewTasks').empty();
+        console.log(result);
+        for(let i=0; i<result.length; i++){
+            console.log(result);
+            $('viewTasks').append(
+                `<tr data-id=${result[i].id}>
+                <td>${result[i].task}</td>
+                <td>${result[i].status}</td>
+                <td><button class="completeButton">Completed</button></td>
+                <td><button class="deleteButton">Delete</button></td>
+        
+                </tr>`
+            )
+        }
     })
 }
 //function addTask -- add new task POST
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -6,10 +6,32 @@ const pool = require('../modules/pool');
 //router.get 
 router.get('/', (req, res) => {
     console.log('will this work');
-    })
+    let queryText = 'SELECT * FROM "weekend-to-do-app" ORDER BY "id";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }) 
+    .catch(error => {
+        console.log('error in getting tasks', error);
+        res.sendStatus(500);
+    });   
+})
     
  
-//router.post
+router.post('/', (req, res) => {
+    let newTask = req.body;
+    console.log('adding task', newTask);
+    let queryText = `INSERT INTO "weekend-to-do-app" ("task", "status")
+                        VALUES ($1, $2);`;
+    pool.query(queryText, [newTask.task, newTask.status])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log('error in adding new task', error);
+            res.sendStatus(500);
+        });
+});
+
 
 //router.delete
 
